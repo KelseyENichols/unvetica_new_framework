de405d7c5cdfdec9107a83bf4f82fe6ef0ed6d35
Spencer Leifheit <spencereleifheit3@gmail.com>
2019-07-02 15:00:04 -0700

    Split of dependencies for future login/registration handler

--- application/controllers/mainPage.php
+++ application/controllers/mainPage.php
@@ -9,12 +9,15 @@ class MainPage extends CI_Controller {
 		
 		$this->load->model('User_model');
 
-		$this->load->view('header');
+		$this->load->view('header_dependencies'); // Loads all of the needed JS Libraries and CSS files
 
-		$this->load->view('home');
+		$this->load->view('login_header'); // Loads the login portion of the header / dynamic to detect login
 
-		$this->load->view('footer');
+		$this->load->view('header'); // Standard site header with navigation
+
+		$this->load->view('home'); // Main content
+
+		$this->load->view('footer'); // Standard Footer that closes all div tags and the body tag
 		
-
 	}
 }
---
+++ application/views/login_header.php
@@ -0,0 +1,27 @@
+<?php
+	defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+			<div>
+				<br/>
+				<table width="100%">
+					<tr>
+						<td width="90%">
+						</td>
+						<td width="5%">
+							<span style="font-size:11px;">
+								<a href -= "#">
+									Sign In
+								</a>
+							</span>
+						</td>
+						<td width="5%">
+							<span style="font-size:11px;">
+								<a href = "#">
+									Register
+								</a>
+							</span>
+						</td>
+					</tr>
+				</table>
+			</div>
