cc72b3ba8262d7fe840da1edf61c79a6561547c2
Spencer Leifheit <spencereleifheit3@gmail.com>
2019-07-02 14:14:51 -0700

    Added files for front-end placeholder

--- application/views/header.php
+++ application/views/header.php
@@ -50,7 +50,83 @@
 
 		<div class="container-fluid">
 
-			<div class = "header">
-				This is the header
+			<div>
+				<br/>
+				<table width="100%">
+					<tr>
+						<td width="90%">
+						</td>
+						<td width="5%">
+							<span style="font-size:11px;">
+								Sign In
+							</span>
+						</td>
+						<td width="5%">
+							<span style="font-size:11px;">
+								Register
+							</span>
+						</td>
+					</tr>
+				</table>
+			</div>
+
+			<div class="col-md-12">
+				
+				<table width="100%">
+					<tr>
+						<td width="18%" align="left">
+							<img src = "../assets/imgs/unvetica_logo.png">
+						</td>
+
+						<td width="15%" align="center">
+							<a href = "#">
+								HOME
+							</a>
+						</td>
+
+						<td width="15%" align="center">
+							<a href = "#">
+								PORTFOLIO
+							</a>
+						</td>
+
+						<td width="15%" align="center">
+							<a href = "#">
+								NEWS
+							</a>
+						</td>	
+
+						<td width="15%" align="center">
+							<a href = "#">
+								WHO WE ARE
+							<a href = "#">
+						</td>											
+
+						<td width="15%" align="center">
+							<a href = "#">
+								CONTACT US
+							</a>
+						</td>
+
+						<td width="2%" align="left">
+							|
+						</td>
+
+						<td width="3%" a;ign="center">
+							<a href = "#">
+								&
+							</a>
+						</td>
+
+						<td width="3%" alig="center">
+							<a href = "#">
+								*
+							</a>
+						</td>						
+
+
+
+					</tr>
+				</table>
 			</div>		
 
--- application/views/home.php
+++ application/views/home.php
@@ -4,6 +4,21 @@
 
 ?>
 
-	<div align = "center">
-		This is the body
+	<div>
+		<br/><br/>
+	</div>
+
+	<div class="col-md-12">
+
+		<table width="100%">
+			<tr>
+				<td>
+					<img src="../assets/imgs/unvetica_banner.jpg">
+						<div style="position:relative; top:-430px; left: 100px;">
+							<img src="../assets/imgs/unvetica_dictionary.png">
+						</div> 	
+				</td>
+			</tr>		
+		</table>
+
 	</div>
